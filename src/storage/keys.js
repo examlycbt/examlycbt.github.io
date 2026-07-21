230207cd63bd13ce9f218cdd01580637baf874d4
Examly <250403224+examlycbt@users.noreply.github.com>
2026-07-21 05:43:01 +0530

    Add constants for local storage keys

---
+++ src/storage/keys.js
@@ -0,0 +1,6 @@
+export const LS_BANKS = 'cbt_banks_v1';
+export const LS_HISTORY = 'cbt_history_v1';
+export const LS_ACTIVE = 'cbt_active_v1';
+export const LS_THEME = 'cbt_theme_v1';
+export const LS_UIPREFS = 'cbt_uiprefs_v1';
+export const LS_REPORTS = 'cbt_reports_v1';
